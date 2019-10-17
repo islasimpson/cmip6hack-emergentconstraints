4219435adfd85bbc59e4a9b1d683732dc322ff65
Flavio Lehner <flehner@ucar.edu>
2019-10-17 08:23:47 -0600

    function to calculate area-weighted average

--- notebooks/wgt_areaave.py
+++ notebooks/wgt_areaave.py
@@ -1,9 +1,11 @@
 import xarray as xr
 import numpy as np
 
-def wgt_areaave(ds,minlat,maxlat,minlon,maxlon):
+def wgt_areaave(ds,var,minlat,maxlat,minlon,maxlon):
+    
     """ calculates area-weighted average in rectangular region given by lat/lon on regular grid
     Args: ds (xarray.Dataset): dataset (needs to be 3-d, e.g. [time x lat x lon])
+          var (str): variable to use
           minlat: -90..90
           maxlat: -90..90
           minlon: 0..360
@@ -12,11 +14,17 @@ def wgt_areaave(ds,minlat,maxlat,minlon,maxlon):
     Import: from wgt_areaave import wgt_areaave as wa
     Usage: ts = wa(da,minlat,maxlat,minlon,maxlon)
     """
-    subset = ds.where((minlon < ds.lon) & (ds.lon < maxlon) & (minlat < ds.lat) & (ds.lat < maxlat), drop=True)
+    
+    # -- subsetting the data and latitudes
+    subset = ds[var].where((minlon < ds.lon) & (ds.lon < maxlon) & (minlat < ds.lat) & (ds.lat < maxlat), drop=True)
     lats = ds.lat.where((minlat < ds.lat) & (ds.lat < maxlat), drop=True)
+    # -- creating latitude weights
     lat_weights = np.cos(np.deg2rad(lats))
+    # -- broadcasting weights to dimensions of data
     tmp1,tmp2 = xr.broadcast(lat_weights,subset,exclude=['lat'])
     weights = tmp1.transpose('time','lat','lon')
+    # -- normalize weights
     weights = weights/np.sum(np.sum(weights,axis=1),axis=1)
+    # -- apply weights and sum up
     ts = np.sum(np.sum((subset*weights),axis=1),axis=1)
     return (ts)
